b84e5ebe806d971d34074621aa72af740da5a284
Jatin917 <jkchandel02@gmail.com>
2025-07-08 19:52:37 +0530

    recording prototype done

--- apps/web/app/libs/supabaseClient.ts
+++ apps/web/app/libs/supabaseClient.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
-  'https://owrbvokdfbnyrgqhlpss.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im93cmJ2b2tkZmJueXJncWhscHNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMwMTU4ODAsImV4cCI6MjA1ODU5MTg4MH0.x4IspthgR_4UGFSGZgeym9d8qC4g7RyLVDZjhqTBkYE'
+  process.env.SUPABASE_URL || '',
+  process.env.SUPABASE_ANON_KEY || '',
 );
 export async function uploadChunkToSupabase(index: number, blob: Blob, sessionToken: string, userId:string) {
     const filePath = `sessions/${sessionToken}/${userId}/chunk-${index}.webm`;
